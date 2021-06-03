3cd25555afd895aabab4dbd21f8f0db9b3259127
Serge Bakharev <serge.bakharev@gmail.com>
2021-06-04 02:23:23 +1000

    correct text template

--- ansible_module2demisto_integration.py
+++ ansible_module2demisto_integration.py
@@ -293,10 +293,10 @@ def main() -> None:
         if integration_def.get('test_command') is not None:
             test_command = integration_def.get('test_command')
             integration_script += '''            # This is the call made when pressing the integration Test button.
-            result = generic_ansible('{demisto_command}', '{test_command}', args, int_params, host_type)
+            result = generic_ansible('%s', '%s', args, int_params, host_type)
 
             return_results(result)
-'''
+''' % (name.lower(), test_command)
         else:
             integration_script += '''            # This is the call made when pressing the integration Test button.
             return_results('This integration does not support testing from this screen. Please refer to the documentation for details on how to perform configuration tests.')
